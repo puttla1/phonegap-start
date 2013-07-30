1f849aabd6d2de894ef55fbf7d44e502435ba939
kiwiluver75 <kiwiluver75@gmail.com>
2013-07-30 09:46:11 -0400

    fixed json

--- www/index.js
+++ www/index.js
@@ -1,11 +1,11 @@
-var a = $.getJSON("http://puneeth.org/docs/test.json", function(data)
+var a = $.getJSON("http://anyorigin.com/get?url=notamdemo.aim.nas.faa.gov/notamWFSTest/json%3FDesignator%3DIAD&callback=?", function(data)
     {
-      var max = data.NOTAMs.Airports.IAD.length;
+      var max = data.contents.NOTAMs.Airports.IAD.length;
       var i;
 
       for(i = 0; i < max; i++)
 {
-    $("#col1").append('<li><div class =\"card\"><p class =\"card-title\">IAD-' + data.NOTAMs.Airports.IAD[i].NOTAMNumber + "</p><p>" + data.NOTAMs.Airports.IAD[i].Domestic + "</p></div></li>");
+    $("#col1").append('<li><div class =\"card\"><p class =\"card-title\">IAD-' + data.contents.NOTAMs.Airports.IAD[i].NOTAMNumber + "</p><p>" + data.contents.NOTAMs.Airports.IAD[i].Domestic + "</p></div></li>");
         }
 
         });
