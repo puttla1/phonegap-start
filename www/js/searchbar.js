db90e3878ef269e63997a828d2341074a36a36a3
kiwiluver75 <kiwiluver75@gmail.com>
2013-08-05 15:24:24 -0400

    added a search type function

---
+++ www/js/searchbar.js
@@ -0,0 +1,19 @@
+$(function() {
+    var opts = $('#optlist option').map(function(){
+          return [[this.value, $(this).text()]];
+            });
+
+
+    $('#airinput').keyup(function(){
+        var rxp = new RegExp($('#airinput').val(), 'i');
+          var optlist = $('#optlist').empty();
+            opts.each(function(){
+                    if (rxp.test(this[1])) {
+                                optlist.append($('<option/>').attr('value', this[0]).text(this[1]));
+                                      }
+                      });
+                  
+    });
+          
+      });
+
