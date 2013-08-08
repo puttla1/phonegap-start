7cbbd0849a81f9a00a6e640a2f1cfe01dcc1ddaf
kiwiluver75 <kiwiluver75@gmail.com>
2013-08-08 11:42:48 -0400

    various fixes

--- www/misc/makehtml.java
+++ www/misc/makehtml.java
@@ -6,17 +6,14 @@ public class makehtml
 	public static void main(String[] args)throws Exception
 	{
 		BufferedReader br = new BufferedReader(new FileReader("input.txt"));
-    PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("output.txt")));
+    PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("out.txt")));
 
 		for(int i = 0; i < 797; i++)
 		{
       String s = br.readLine();
       if(s.length() == 0 || s.length() == 1 || s.indexOf("All airports") > 0 || s.indexOf("return to top") > 0) continue;
-      StringTokenizer st = new StringTokenizer(s ,  "(");
-      String full = st.nextToken();
-      String code = st.nextToken();
 
-      System.out.println(code.substring(0,3));
+      System.out.println("<li><a =href=\"index.html\">" + s + "</a></li>");
 
 		}
 	}
