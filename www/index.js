74d38eb69bb78f0ca613f34f23cd265658706543
kiwiluver75 <kiwiluver75@gmail.com>
2013-08-14 10:26:29 -0400

    made polygon default

--- www/index.js
+++ www/index.js
@@ -1,7 +1,12 @@
 function getPolygonUrl(geo, lat, longi)
 {
   var coords = geo.split(",");
-  var s = "http://maps.googleapis.com/maps/api/staticmap?center=" + lat + "" + longi + "&zoom=12&size=400x400&maptype=roadmap&sensor=false&path=color:red|weight:5|fillcolor:white";
+  var width = document.getElementById("card1").clientWidth * .7;
+  width = Math.round(width);
+  var height = document.getElementById("card1").clientHeight * 2;
+  height = Math.round(height);
+  
+  var s = "http://maps.googleapis.com/maps/api/staticmap?center=" + lat + "" + longi + "&zoom=12&size=" + width + "x" + height + "&maptype=roadmap&sensor=false&path=color:red|weight:5|fillcolor:white";
   var i;
   for(i =0; i < coords.length; i++)
   {
@@ -29,6 +34,15 @@ google.maps.Polygon.prototype.getBounds = function() {
     }
     return bounds;
 }
+function hidePoint(geometry, id)
+{
+  var arr = geometry.split("(");
+  if(geometry == "null" || arr[0] == "POINT" || arr[0] == "MULTIPOLYGON")
+  {
+    var elem = document.getElementById(id);
+    elem.style.display = "none";
+  }
+}
 
 function findUrl(code, geometry, lat, longi)
 {
@@ -38,12 +52,17 @@ function findUrl(code, geometry, lat, longi)
 
   if(arr[0] == "POINT")
   {
-    return getPointUrl(arr[1].substring(0,arr[1].length-1));
+     return getPointUrl(arr[1].substring(0,arr[1].length-1));
   }
 
   else if(arr[0] == "POLYGON")
   {
      return getPolygonUrl(arr[2].substring(0,arr[2].length-2), lat, longi);
+  }
+
+  else if(arr[0] == "MULTIPOLYGON")
+  {
+    return "";
   }
 
   else return "";
@@ -55,11 +74,7 @@ function initPoly(arr, id, lat, longi)
     var mapOptions = 
     {
       zoom: 12,
-      center: myLatLng,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-  var airmap;
+      center: myLatLng, mapTypeId: google.maps.MapTypeId.ROADMAP }; var airmap;
 
   var map = new google.maps.Map(document.getElementById(id), mapOptions);
 
@@ -161,7 +176,8 @@ var a = $.getJSON("http://anyorigin.com/get?url=puneeth.org/notamWFS/IAD.json&ca
       for(i = 0; i < max; i++)
       {
         var imgurl = findUrl(code, data.contents.NOTAMs.Airports.IAD[i].Geometry, lat, longi);
-        $("#col1").append('<li><div onclick=\"$(\'#img' + (i+1) + '\').toggle();$(\'#title'+ (i+1) + '\').toggle();$(\'#content'+(i+1)  + '\').toggle();\" class =\"card\"><p class =\"card-title\" id=\"title' + (i+1) + '\" >IAD-' + data.contents.NOTAMs.Airports.IAD[i].NOTAMNumber + "</p><p id=\"content" + (i+1) + "\">" + data.contents.NOTAMs.Airports.IAD[i].Domestic + "</p> <img style=\"display:none; margin-left:auto; margin-right:auto;\" id=\"img" + (i+1) + "\" src=\""+imgurl+ "\"> </div></li>");
+        $("#col1").append('<li><div onclick=\"$(\'#img' + (i+1) + '\').toggle();$(\'#title'+ (i+1) + '\').toggle();$(\'#content'+(i+1)  + '\').toggle();\" class =\"card\" id=\"card'+ (i+1) + '\"><p class =\"card-title\" id=\"title' + (i+1) + '\" >IAD-' + data.contents.NOTAMs.Airports.IAD[i].NOTAMNumber + "</p><p id=\"content" + (i+1) + "\">" + data.contents.NOTAMs.Airports.IAD[i].Domestic + "</p> <img style=\"margin-left:auto;margin-right:auto;\" id=\"img" + (i+1) + "\"  src=\""+imgurl+ "\"> </div></li>");
+        hidePoint(data.contents.NOTAMs.Airports.IAD[i].Geometry, "img" + (i+1));
         //$("#col1").append('<li><div onclick=\"$(\'#img' + (i+1) + '\').toggle();$(\'#title'+ (i+1) + '\').toggle();$(\'#content'+(i+1)  + '\').toggle();\" class =\"card\"><p class =\"card-title\" id=\"title' + (i+1) + '\" >IAD-' + data.contents.NOTAMs.Airports.IAD[i].NOTAMNumber + "</p><p id=\"content" + (i+1) + "\">" + data.contents.NOTAMs.Airports.IAD[i].Domestic + "</p> <div id=\"img" + (i+1) + "\" style=\"display:block;position:absolute;left:-1000px;width:25%; height:7em;\"> </div></li>");
         //makeShape(data.contents.NOTAMs.Airports.IAD[i].Geometry, "img" + (i+1), lat, longi);
       }
