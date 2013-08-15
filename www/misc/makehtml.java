be85c96bd37333660968fa24859fb09bb75e17b7
kiwiluver75 <kiwiluver75@gmail.com>
2013-08-15 11:18:13 -0400

    Made sure that notams exist for all selections

--- www/misc/makehtml.java
+++ www/misc/makehtml.java
@@ -7,7 +7,14 @@ public class makehtml
 	{
 		BufferedReader br = new BufferedReader(new FileReader("input.txt"));
     PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("out.txt")));
+     
+		BufferedReader b = new BufferedReader(new FileReader("designators.txt"));
 
+    ArrayList<String> list = new ArrayList<String>();
+    for(int i = 0; i < 812; i++)
+      list.add(b.readLine());
+    b.close();
+    int count = 0;
 		for(int i = 0; i < 797; i++)
 		{
       String s = br.readLine();
@@ -16,9 +23,11 @@ public class makehtml
       String temp = st.nextToken();
       String id = st.nextToken();
       id = id.substring(0, id.length() - 1);
-
-      System.out.println("<li><a id=\"" + id + "\" rel=\"external\" href=\"index.html\">" + s + "</a></li>");
-
+      if(list.contains(id))
+      {
+        count ++;
+       System.out.println("<li><a id=\"" + id + "\" onclick=\"storeID(this)\" rel=\"external\" href=\"index.html\">" + s + "</a></li>");
+      }
 		}
 	}
 }
