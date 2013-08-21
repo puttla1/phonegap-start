ae77826e0b16c7404a7c7c30489e34488c8de191
kiwiluver75 <kiwiluver75@gmail.com>
2013-08-20 23:45:14 -0400

    Changed confirm to alert

--- www/index.js
+++ www/index.js
@@ -183,8 +183,8 @@ else if (typeof window.localStorage.getItem('airportcode')!== 'undefined' &&  wi
 
      }
 else{
-if(confirm("No preferred airport code has been set. Please set up a preferred code in Settings -> Set preferred airport. Until then IAD will be the default airport"))
-  window.location.href = 'settings.html';
+alert("No preferred airport code has been set. Please set up a preferred code in Settings -> Set preferred airport.");
+window.location.href = 'settings.html';
 code = "IAD";
 }
 
